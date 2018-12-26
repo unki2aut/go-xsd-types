3b444182ab5336de155e89c7745de0bcc74bcf91
Martin Fillafer <martin.fillafer@bitmovin.com>
2018-12-26 10:18:01 +0100

    Add DateTime parsing and marshalling

---
+++ datetime.go
@@ -0,0 +1,61 @@
+package xsd
+
+import (
+	"encoding/xml"
+	"time"
+)
+
+type DateTime time.Time
+
+const (
+	DateTimeFormat     = "2006-01-02T15:04:05.999999999-07:00"
+	DateTimeNoTimezone = "2006-01-02T15:04:05.999999999"
+)
+
+func DateTimeFromString(str string) (*DateTime, error) {
+	var (
+		val time.Time
+		err error
+		z   = str[len(str)-6]
+	)
+
+	if z == '+' || z == '-' {
+		val, err = time.Parse(DateTimeFormat, str)
+	} else {
+		val, err = time.Parse(DateTimeNoTimezone, str)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return (*DateTime)(&val), nil
+}
+
+func (dt *DateTime) String() string {
+	return (*time.Time)(dt).Format(DateTimeFormat)
+}
+
+func (dt *DateTime) MarshalXMLAttr(name xml.Name) (xml.Attr, error) {
+	if dt == nil {
+		return xml.Attr{}, nil
+	}
+	return xml.Attr{Name: name, Value: dt.String()}, nil
+}
+
+func (dt *DateTime) UnmarshalXMLAttr(attr xml.Attr) error {
+	val, err := DateTimeFromString(attr.Value)
+	if err != nil {
+		return err
+	}
+
+	*dt = *val
+	return nil
+}
+
+// check interfaces
+var (
+	dt                     = DateTime{}
+	_  xml.MarshalerAttr   = &dt
+	_  xml.UnmarshalerAttr = &dt
+)
---
+++ datetime_test.go
@@ -0,0 +1,69 @@
+package xsd
+
+import (
+	"bytes"
+	"encoding/xml"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDateTime(t *testing.T) {
+	var (
+		val *DateTime
+		err error
+	)
+
+	val, err = DateTimeFromString("2015-09-07T05:45:54+01:00")
+	assert.Nil(t, err)
+	assert.Equal(t, "2015-09-07T05:45:54+01:00", val.String())
+
+	val, err = DateTimeFromString("2015-09-07T05:45:54-01:00")
+	assert.Nil(t, err)
+	assert.Equal(t, "2015-09-07T05:45:54-01:00", val.String())
+
+	val, err = DateTimeFromString("2015-09-07T05:45:54")
+	assert.Nil(t, err)
+	assert.Equal(t, "2015-09-07T05:45:54+00:00", val.String())
+
+	val, err = DateTimeFromString("2015-09-07T05:45:54Z")
+	assert.NotNil(t, err)
+}
+
+type DateTimeAttr struct {
+	DateTime *DateTime `xml:"dateTime,attr"`
+}
+
+func TestDateTime_UnmarshalXMLAttr(t *testing.T) {
+	dta := DateTimeAttr{}
+	err := xml.Unmarshal([]byte(`<foo dateTime="2015-09-07T05:45:54+00:00"></foo>`), &dta)
+	assert.Nil(t, err)
+	assert.NotNil(t, dta.DateTime)
+	assert.Equal(t, "2015-09-07T05:45:54+00:00", (*dta.DateTime).String())
+}
+
+func TestDateTime_MarshalXMLAttr(t *testing.T) {
+	val, err := DateTimeFromString("2015-09-07T05:45:54")
+	assert.Nil(t, err)
+	dur := DateTimeAttr{DateTime: val}
+
+	b := new(bytes.Buffer)
+	e := xml.NewEncoder(b)
+	err = e.Encode(dur)
+
+	assert.Nil(t, err)
+	assert.Equal(t, `<DateTimeAttr dateTime="2015-09-07T05:45:54+00:00"></DateTimeAttr>`, b.String())
+}
+
+func BenchmarkDateTimeFromString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = DateTimeFromString("2017-08-16T13:07:00.09251+02:00")
+	}
+}
+
+func BenchmarkDateTime_String(b *testing.B) {
+	dateTime, _ := DateTimeFromString("2017-08-16T13:07:00.09251+02:00")
+
+	for i := 0; i < b.N; i++ {
+		_ = dateTime.String()
+	}
+}
